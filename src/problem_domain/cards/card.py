ea223098ae576eee269bf77802ebdfcc575ff3a2
Guilherme <guilhermepierri8@gmail.com>
2024-12-05 19:11:55 -0300

    teste

--- src/problem_domain/cards/card.py
+++ src/problem_domain/cards/card.py
@@ -3,12 +3,12 @@ from abc import ABC, abstractmethod
 class Card(ABC):
 
     def __init__(self, type: str, name: str, life: int, damage: int, glyph: None, cost: None):
-        self.type = type
-        self.name = name
-        self.life = life
-        self.damage = damage
-        self.glyph = glyph
-        self.cost = cost
+        self._type = type
+        self._name = name
+        self._life = life
+        self._damage = damage
+        self._glyph = glyph
+        self._cost = cost
 
     @abstractmethod
     def attack(self, target):
