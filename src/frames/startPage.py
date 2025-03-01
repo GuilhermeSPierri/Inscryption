80d1a78b231298ea0559f32b0e61e22ce12f5987
AugustoSchw <augustoschweitzer2@gmail.com>
2025-02-28 21:10:35 -0300

    tela de inicio alterada

--- src/frames/startPage.py
+++ src/frames/startPage.py
@@ -20,15 +20,15 @@ class StartPage(Page):
         bg_label = tk.Label(self, image=self.bg_photo)
         bg_label.place(relwidth=1, relheight=1)  # Faz com que o fundo cubra toda a tela
 
-        label = tk.Label(self, text="Inscryption", font=self.custom_font_title, fg="green", bg="black")
-        label.place(relx=0.5, rely=0.4, anchor="center")
+       # label = tk.Label(self, text="Inscryption", font=self.custom_font_title, fg="green", bg="black")
+        #label.place(relx=0.5, rely=0.4, anchor="center")
 
-        game_button = Button(self, "Iniciar Partida", 20, 5, "start_match", self.controller,font=self.custom_font_buttons)
-        game_button.place(relx=0.5, rely=0.6, anchor="center")
+        game_button = Button(self, "Iniciar Partida", 18, 3, "start_match", self.controller,font=self.custom_font_buttons)
+        game_button.place(relx=0.5, rely=0.67, anchor="center")
 
-        deck_button = Button(self, "Criar Deck", 20, 5, "show_frame", self.controller, ("DeckPage",), font=self.custom_font_buttons)
-        deck_button.place(relx=0.5, rely=0.7, anchor="center")
+        deck_button = Button(self, "Criar Deck", 18, 3, "show_frame", self.controller, ("DeckPage",), font=self.custom_font_buttons)
+        deck_button.place(relx=0.5, rely=0.77, anchor="center")
 
-        exit_button = Button(self, "Sair do Jogo", 20, 5, "leave_game", self.controller, font=self.custom_font_buttons)
-        exit_button.place(relx=0.5, rely=0.8, anchor="center")
+        exit_button = Button(self, "Sair do Jogo", 18, 3, "leave_game", self.controller, font=self.custom_font_buttons)
+        exit_button.place(relx=0.5, rely=0.87, anchor="center")
 
