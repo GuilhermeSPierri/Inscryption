8e5b8a45e571a66fbe853c7fa4ad9a69cd273adf
AugustoSchw <augustoschweitzer2@gmail.com>
2025-03-03 15:55:54 -0300

    imagens de lobo e urso atualizadas

--- src/frames/startPage.py
+++ src/frames/startPage.py
@@ -18,12 +18,12 @@ class StartPage(Page):
         bg_label = tk.Label(self, image=self.bg_photo)
         bg_label.place(relwidth=1, relheight=1)  # Faz com que o fundo cubra toda a tela
 
-        game_button = Button(self, "Iniciar  Partida", 18, 3, "start_match", self.controller,font=self.custom_font_buttons)
+        game_button = Button(self, "Iniciar  Partida", 18, 2, "start_match", self.controller,font=self.custom_font_buttons)
         game_button.place(relx=0.5, rely=0.67, anchor="center")
 
-        deck_button = Button(self, "Criar  Deck", 18, 3, "show_frame", self.controller, ("DeckPage",), font=self.custom_font_buttons)
+        deck_button = Button(self, "Criar  Deck", 18, 2, "show_frame", self.controller, ("DeckPage",), font=self.custom_font_buttons)
         deck_button.place(relx=0.5, rely=0.77, anchor="center")
 
-        exit_button = Button(self, "Sair  do  Jogo", 18, 3, "leave_game", self.controller, font=self.custom_font_buttons)
+        exit_button = Button(self, "Sair  do  Jogo", 18, 2, "leave_game", self.controller, font=self.custom_font_buttons)
         exit_button.place(relx=0.5, rely=0.87, anchor="center")
 
